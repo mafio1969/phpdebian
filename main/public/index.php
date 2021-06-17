6b473fec23ed4cc8f0d6ac63a7bbb06872ba90fb
Mariusz Franciszczak <mariusz.franciszczak@ccfound.com>
2021-06-17 18:28:56 +0200

    Initial commit

---
+++ main/public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mariusz Franciszczak using keopx/apache-php:latest</title>
+</head>
+<body>
+<H4>environment</H4>
+<p>APP WORKING: Debian GNU/Linux 10 (buster) PHP 8.0.3 (cli) Apache/2.4.38 (Debian), PORT 8080 , CRON RUN, ready to use symfony-cli) pppppp</p>
+<?= phpinfo() ?>
+</body>
+</html>
